ffd617ec731b02d9536c4e849a541af70f4b96ec
Stas Ivanov <stasivanov@MacBook-Air-Stas.local>
2021-11-19 09:29:37 +0200

    Changed HelloJava.java

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -4,5 +4,6 @@ package main.java;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("This is our second commit");
     }
 }
