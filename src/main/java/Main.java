1febcfe25b96a4743f7bab042082254f11aa33d2
Stas Ivanov <stasivanov@MacBook-Air-Stas.local>
2021-11-26 17:22:40 +0200

    test commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+package main.java;
+
+
+public class Main {
+    public static void main(String[] args) {
+        float applesDima = 3.5f;
+        float applesAnia = 2.5f;
+        float sum = applesDima + applesAnia;
+        char symbol = 214;
+        System.out.println(sum + "\n" + symbol);
+    }
+}
